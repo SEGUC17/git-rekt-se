e6803106297681ef880e4000d30c7d39a99d9a2d
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-29 21:30:38 +0200

    feat: Branch DB Model

---
+++ app/models/service/Branch.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const locations = require('../../seed/service/locations');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Branch Schema
+ */
+
+const branchSchema = mongoose.Schema({
+  _business: {
+    type: Schema.Types.ObjectId,
+    ref: 'Business',
+  },
+  location: {
+    type: String,
+    required: true,
+    enum: locations,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Branch', branchSchema);
---
+++ app/seed/service/locations.js
@@ -0,0 +1,13 @@
+/**
+ * List of avaliable locations
+ */
+
+const locations = [
+  'Nasr City',
+  '6th October City',
+  'Tagamo3',
+  'Zamalek',
+  'Sheikh Zayed',
+];
+
+module.exports = locations;
