bcc97459471813c89bd9bd6cb8b43c451dad4b4c
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-29 21:51:46 +0200

    feat: Offering DB Model

--- app/models/service/Offering.js
+++ app/models/service/Offering.js
@@ -1,1 +1,32 @@
+const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
+/**
+ * Offering Schema
+ */
+
+const offeringSchema = Schema({
+  _service: {
+    type: Schema.Types.ObjectId,
+    ref: 'Service',
+  },
+  branch: {
+    type: Schema.Types.ObjectId,
+    ref: 'Branch',
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Offering', offeringSchema);
