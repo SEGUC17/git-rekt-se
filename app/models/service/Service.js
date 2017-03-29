7bafee75bb038577194777532f3b34b72c6c8ee3
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-29 23:02:07 +0200

    feat: Add DB Service Model

---
+++ app/models/service/Service.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Service Schema
+ */
+
+const serviceSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  shortDescription: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  _business: {
+    type: Schema.Types.ObjectId,
+    ref: 'Business',
+    required: true,
+  },
+  branches: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Branch',
+    required: true,
+  }],
+  offerings: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Offering',
+  }],
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Review',
+  }],
+  gallery: [{
+    path: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  }],
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
