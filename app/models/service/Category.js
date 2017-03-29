16bbd26fdeefd36f026aaafed885e7f2a5559312
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-29 21:42:12 +0200

    feat: Category DB Model

---
+++ app/models/service/Category.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const locations = require('../../seed/service/locations');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Category Schema
+ */
+
+const categorySchema = Schema({
+  type: {
+    type: String,
+    required: true,
+    enums: ['Service', 'Business'],
+  },
+  title: {
+    type: String,
+    required: true,
+    enum: locations,
+  },
+  icon: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Category', categorySchema);
