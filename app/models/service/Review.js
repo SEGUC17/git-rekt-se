b20fe2ec89e487d3fdd4111c2e42d6519d219880
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-29 21:16:01 +0200

    feat: Add Review DB Model

---
+++ app/models/service/Review.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Review Schema
+ */
+
+const reviewSchema = mongoose.Schema({
+  _client: {
+    type: Schema.Types.ObjectId,
+    ref: 'Client',
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
