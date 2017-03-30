b2571d4200d834b130a108c35f1a4a78c05e9d3b
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-30 20:05:42 +0200

    feat: Add Basic Client Signup tests

---
+++ test/api/client/clientAuthTests.js
@@ -0,0 +1,72 @@
+/**
+ * API Testing Template
+ */
+
+const mongoose = require('mongoose');
+const chai = require('chai');
+const supertest = require('supertest');
+const app = require('../../../app/app');
+const Client = require('../../../app/models/client/Client');
+const clients = require('../../../app/seed/client/clientSeed');
+
+mongoose.Promise = Promise;
+
+/**
+ * Client Signup Suite
+ */
+
+describe('Client Signup API', () => {
+  let req;
+
+  before((done) => {
+    Client.collection.drop();
+    Client.ensureIndexes(done);
+  });
+
+  beforeEach(() => {
+    req = supertest(app)
+      .post('/api/v1/auth/signup');
+  });
+
+  /**
+   * Register a new client
+   */
+
+  it('should register a new client', (done) => {
+    const client1 = clients[0];
+    req.send(client1)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        /**
+         * Error happend with request, fail the test
+         * with the error message.
+         */
+
+        if (err) {
+          done(err);
+        } else {
+          Client.find({
+            email: client1.email,
+          }, (finderr, data) => {
+            if (finderr) {
+              done(finderr);
+            } else {
+              chai.expect(data.length)
+                .to.equal(1);
+              done();
+            }
+          });
+        }
+      });
+  });
+
+  it('should not allow same email for multiple users', (done) => {
+    const client1 = clients[0];
+    req.send(client1)
+      .expect('Content-Type', /json/)
+      .expect(400, {
+        error: 'Client account already exists.',
+      }, done);
+  });
+});
