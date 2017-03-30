4d80ad7705a587daa87252d3b990e4c773129897
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-30 21:05:28 +0200

    feat: Client signup form validation schema
    References #7.

--- app/routes/api/v1/client/auth.js
+++ app/routes/api/v1/client/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const validationSchemas = require('../../../../services/validation');
 
 const router = express.Router();
 
@@ -8,14 +10,22 @@ const router = express.Router();
  */
 
 router.use(bodyParser.json());
+router.use(expressValidator({}));
 
 /**
  * Client signup route
  */
 
 router.post('/signup', (req, res) => {
+  req.checkBody(validationSchemas.clientSignupValidation);
+
+  req.getValidationResult()
+    .then((result) => {
+      console.log(result.array());
+    });
+
   res.send({
-    message: 'This should be fun.',
+    message: 'ok',
   });
 });
 
