f7e19f7528c4353dece6a26a4ef8c430365925f3
Omarkojak <omarkojaks@gmail.com>
2017-04-01 01:30:54 +0200

    feat: Visitor View Related Business

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,12 +1,13 @@
 const clientAuthAPI = require('./api/v1/client/auth');
 const businessAuthAPI = require('./api/v1/business/auth');
 const adminAuthAPI = require('./api/v1/admin/auth');
+const visitorAPI = require('./api/v1/visitor/index');
 
 module.exports = (app) => {
   /**
    * Visitor Routes
    */
-
+  app.use('api/v1/visitor', visitorAPI);
   /**
    * Client Routes
    */
