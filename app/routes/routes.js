0db0a29db81640814a31becd812ef9a77f1928f5
Abdelrahman A. Elshabrawy <abdobassiony996@hotmail.com>
2017-04-01 11:21:52 +0200

    feat: CREATE service image

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,22 @@
 const clientAuthAPI = require('./api/v1/client/auth');
 const businessAuthAPI = require('./api/v1/business/auth');
 const adminAuthAPI = require('./api/v1/admin/auth');
+var multer = require('multer');
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './service_gallery/');
+  },
+  filename: function (req, file, cb) {
+    const buf = crypto.randomBytes(16);
+    cb(null, Date.now() + buf.toString('hex') + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
 
 module.exports = (app) => {
   /**
@@ -21,6 +37,28 @@ module.exports = (app) => {
    * Admin Routes
    */
   app.use('/api/v1/admin/auth', adminAuthAPI);
+
+  router.get("/addServiceImage", function (req, err) {
+    res.render("addServiceImage");
+  });
+
+  router.post("/addServiceImage", uploads.any(), function (req, res, next) { //ensureauthenticated
+    var Image = ({
+      path: req.files[0],
+      description: req.body.description,
+    }).save((err, data) => {
+      if (err) {
+        throw err;
+      }
+      req.service.gallery.push(data);
+      req.service.save((err) => {
+
+        req.flash("info", "Image added successfully!");
+        res.redirect("/addServiceImage");
+      })
+    });
+  });
+
 
   /**
    * Service Routes
