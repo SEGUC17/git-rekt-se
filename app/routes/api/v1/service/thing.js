044eb7fb01e5fa0748e447a1538af12a5986c417
Abdelrahman A. Elshabrawy <abdobassiony996@hotmail.com>
2017-04-01 23:45:43 +0200

    feat: service gallery image create API testing

--- app/routes/api/v1/service/thing.js
+++ app/routes/api/v1/service/thing.js
@@ -31,6 +31,7 @@ Service Image CRUD section
 Service Image Create
 */
 router.use(bodyParser.json());
+/* UNCOMMENT WHEN AUTHENTICATION IS READY
 router.post('/addServiceImage/:id', upload.any(), (req, res, next) => { // ensureauthenticated
   const image = ({
     path: req.files[0],
@@ -41,8 +42,8 @@ router.post('/addServiceImage/:id', upload.any(), (req, res, next) => { // ensur
   })
     .exec((err, service) => {
       if (service) {
-         /* match logged in business to service._businesss
-          to check whether logged in business matches the service provider.*/
+         // match logged in business to service._businesss
+         // to check whether logged in business matches the service provider.
         if (Service._business === req.user._id) {
           service.gallery.push(image);
           Service.save((err2) => {
@@ -62,6 +63,38 @@ router.post('/addServiceImage/:id', upload.any(), (req, res, next) => { // ensur
             message: 'This service does not belong to your business!',
           });
         }
+      } else {
+        res.json({
+          message: 'Invalid service!',
+        });
+        next(err);
+      }
+    });
+});
+*/
+router.post('/addServiceImage/:id', upload.any(), (req, res, next) => { // ensureauthenticated
+  const image = ({
+    path: req.files[0],
+    description: req.body.description,
+  });
+  Service.findOne({
+    _id: req.params.id,
+  })
+    .exec((err, service) => {
+      if (service) {
+        service.gallery.push(image);
+        Service.save((err2) => {
+          if (err) {
+            next(err);
+          } else
+          if (err2) {
+            next(err2);
+          } else {
+            res.json({
+              message: 'Image added succesfully!',
+            });
+          }
+        });
       } else {
         res.json({
           message: 'Invalid service!',
