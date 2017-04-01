24740ba111cbd3e70253db20268d45668b38fa10
Omarkojak <omarkojaks@gmail.com>
2017-04-01 18:38:31 +0200

    feat: adding count to the related businesses

--- app/routes/api/v1/visitor/viewRelatedBusinesses.js
+++ app/routes/api/v1/visitor/viewRelatedBusinesses.js
@@ -17,34 +17,46 @@ router.use(bodyParser.json);
 router.get('business/category/:id/:offset', (req, res, next) => {
   const categoryId = req.params.id;
   const offset = req.params.offset;
-
-  Business.find({
-    categories: {
-      $not: {
-        $size: 0,
-      },
-      $in: {
-        categoryId,
-      },
+  Business.count({ categories: {
+    $not: {
+      $size: 0,
     },
-  }, {
-    _deleted: 0,
-    gallery: 0,
-    password: 0,
-  }, {
-    skip: offset * 10,
-    limit: 10,
-  })
+    $in: {
+      categoryId,
+    },
+  } }, (err, cnt) => {
+    if (err) {
+      next(err);
+    }
+    Business.find({
+      categories: {
+        $not: {
+          $size: 0,
+        },
+        $in: {
+          categoryId,
+        },
+      },
+    }, {
+      _deleted: 0,
+      gallery: 0,
+      password: 0,
+    }, {
+      skip: offset * 10,
+      limit: 10,
+    })
     .populate('branches', 'location address')
     .populate('categories', 'title icon')
-    .exec((err, businesses) => {
-      if (err) {
+    .exec((err2, businesses) => {
+      if (err2) {
         return next(err);
       }
       return res.json({
+        count: cnt,
         results: businesses,
       });
     });
+  });
 });
 
 
