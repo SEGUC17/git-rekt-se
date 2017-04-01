1d9f78cc0a8975d10a80b60ba55ed23fd19cb7fc
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-01 23:12:37 +0200

    fix: Update Client Schema to return Promise

--- app/models/client/Client.js
+++ app/models/client/Client.js
@@ -80,9 +80,15 @@ clientSchema.pre('save', function preSave(done) {
 /**
  * Check If {guess} matches the user password.
  */
-clientSchema.methods.checkPassword = (guess, done) => {
-  bcrypt.compare(guess, this.password, (err, matching) => {
-    done(err, matching);
+
+clientSchema.methods.checkPassword = function checkPassword(guess) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(guess, this.password, (err, matching) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(matching);
+    });
   });
 };
 
