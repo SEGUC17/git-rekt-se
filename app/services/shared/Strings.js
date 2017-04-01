4b8bef44e6704ca550d25fdb507619acaa27d72a
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-01 11:27:03 +0200

    feat: Business Signup Form Validation
    References #17.

--- app/services/shared/Strings.js
+++ app/services/shared/Strings.js
@@ -3,6 +3,12 @@
  */
 
 const bussinessValidationErrors = {
+  emptyEmail: 'Email is a required field.',
+  invalidEmail: 'Invalid Email.',
+  emptyName: 'Name is a required fielD.',
+  emptyMobile: 'Mobile is a required field.',
+  emptyDescription: 'Description is a required field.',
+  invalidMobile: 'Mobile must be in this format 01xxxxxxxxx',
   passwordRequired: 'Password is a required field.',
   passwordLength: 'Password length must be between 8 and 15 and contains at least one number.',
   passwordMismatch: 'Password and Password Confirmation must match.',
