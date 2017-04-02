f245945753a24f4184d0097850d92c7173fe1acb
hady1100 <hadyyasser23@gmail.com>
2017-04-02 11:47:12 +0200

    feat: added client reset password

--- app/routes/api/v1/client/auth.js
+++ app/routes/api/v1/client/auth.js
@@ -6,8 +6,17 @@ const Mailer = require('../../../../services/shared/Mailer');
 const Client = require('../../../../models/client/Client');
 const ClientAuthenticator = require('../../../../services/client/ClientAuthenticator');
 const Strings = require('../../../../services/shared/Strings');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
 
 const router = express.Router();
+mongoose.Promise = Promise;
+
+require('dotenv')
+  .config();
+
+const JWT_KEY = process.env.JWT_KEY;
 
 /**
  * Body Parser Middleware
@@ -95,6 +104,63 @@ router.post('/confirmation/send', (req, res, next) => {
       }
     });
 });
+
+/**
+ * Client reset password
+ */
+
+router.post('/reset', (req, res, next) => {
+  const resetToken = req.body.token;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+
+    // Check If any required field are missing
+  if (!(password && confirmPassword && resetToken)) {
+    next(Strings.INCOMPLETE_INFORMATION);
+  }
+
+    // Check if password and confirmation mismatch
+  if (password !== confirmPassword) {
+    next(Strings.PASSWORD_MISMATCH);
+  }
+
+    // Check that password satisfies password conditions
+    // The password must be at least 8 characters and includes at least a digit
+    //  and a special character.
+    // http://stackoverflow.com/questions/19605150/
+
+  // const passwordRegex = /(?=.*\d)(?=.*[$@$!%*#?.&])[A-Za-z\d$@$!%*#?.&]{8,}$/;
+  // if (!passwordRegex.test(password)) {
+  //   next(Strings.INVALID_PASSWORD);
+  // }
+
+  jwt.verify(resetToken, JWT_KEY, (payload) => {
+    const email = payload.email;
+    const creationDate = new Date(parseInt(payload.iat, 10) * 1000);
+
+    Client.findOne({
+      email,
+      passwordResetTokenDate: {
+        $lte: creationDate,
+      },
+    }, (client) => {
+      if (!client) {
+        console.log(1);
+        next(Strings.INVALID_RESET_TOKEN);
+      }
+
+      client.passwordResetTokenDate = undefined; // Disable the token
+      client.passwordChangeDate = Date.now(); // Invalidate Login Tokens
+      client.password = password; // Reset password
+
+      client.save().exec().then(() => res.json({
+        message: Strings.PASSWORD_RESET_SUCCESS,
+      }));
+    }).catch(err => next([err]));
+  }).catch(err => next([Strings.INVALID_RESET_TOKEN, err]));
+});
+
 
 /**
  * Confirm Email Route
