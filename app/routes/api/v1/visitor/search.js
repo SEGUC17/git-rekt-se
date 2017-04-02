a8ec3be23593d4b17a6fc4c38ca3b56659316d1e
Medo3G <medo3g@yahoo.com>
2017-04-02 10:08:56 +0200

    fix:Correctly execute query

--- app/routes/api/v1/visitor/search.js
+++ app/routes/api/v1/visitor/search.js
@@ -11,100 +11,99 @@ mongoose.Promise = Promise;
  */
 
 router.get('/search', (req, res, next) => {
-  const inputQuery = req.query;
-  // Build up query
-  const offset = (inputQuery.offset) ? inputQuery.offset : 0;
-  const mongooseQuery = {
-    _deleted: false,
-  };
-  if (inputQuery.name) {
-    mongooseQuery.push({
-      name: new RegExp(inputQuery.name, 'i'),
-    });
-  }
-  if (inputQuery.rating) {
-    mongooseQuery.push({
-      _avgRating: {
-        $gte: inputQuery.rating,
-      },
-    });
-  }
-  // Check if query needs to check the offerings of the service
-  if (inputQuery.min || inputQuery.max || inputQuery.location) {
-    mongooseQuery.push({
-      offerings: {
-        $elemMatch: {},
-      },
-    });
-  }
-  if (inputQuery.min || inputQuery.max) {
-    mongooseQuery.offerings.$elemMatch.push({
-      price: {},
-    });
-  }
-  if (inputQuery.min) {
-    mongooseQuery.offerings.$elemMatch.price.push({
-      $gte: inputQuery.min,
-    });
-  }
-  if (inputQuery.max) {
-    mongooseQuery.offerings.$elemMatch.price.push({
-      $lte: inputQuery.max,
-    });
-  }
-  if (inputQuery.location) {
-    mongooseQuery.offerings.$elemMatch.push({
-      business: {
-        location: inputQuery.location,
-      },
-    });
-  }
-  // Query to execute
-  const fullQuery = Service
-    .populate({
-      path: 'offerings',
-      match: {
+      const inputQuery = req.query;
+      // Build up query
+      const offset = (inputQuery.offset) ? inputQuery.offset : 0;
+      const mongooseQuery = {
         _deleted: false,
-      },
-      populate: {
-        path: 'branches',
-        match: {
-          _deleted: false,
-        },
-      },
-    })
-    .find(mongooseQuery)
-    .skip(offset * 10)
-    .limit(10);
-  // Executing
-  fullQuery.count()
-    .exec()
-    .then((cnt) => {
-      fullQuery.populate('_business categories')
-        .select('name shortDescription _business.name _avgRating categories.title')
+      };
+      if (inputQuery.name) {
+        mongooseQuery.push({
+          name: new RegExp(inputQuery.name, 'i'),
+        });
+      }
+      if (inputQuery.rating) {
+        mongooseQuery.push({
+          _avgRating: {
+            $gte: inputQuery.rating,
+          },
+        });
+      }
+      // Check if query needs to check the offerings of the service
+      if (inputQuery.min || inputQuery.max || inputQuery.location) {
+        mongooseQuery.push({
+          offerings: {
+            $elemMatch: {},
+          },
+        });
+      }
+      if (inputQuery.min || inputQuery.max) {
+        mongooseQuery.offerings.$elemMatch.push({
+          price: {},
+        });
+      }
+      if (inputQuery.min) {
+        mongooseQuery.offerings.$elemMatch.price.push({
+          $gte: inputQuery.min,
+        });
+      }
+      if (inputQuery.max) {
+        mongooseQuery.offerings.$elemMatch.price.push({
+          $lte: inputQuery.max,
+        });
+      }
+      if (inputQuery.location) {
+        mongooseQuery.offerings.$elemMatch.push({
+          business: {
+            location: inputQuery.location,
+          },
+        });
+      }
+      // Query to execute
+      const fullQuery = Service
+        .populate({
+          path: 'offerings',
+          match: {
+            _deleted: false,
+          },
+          populate: {
+            path: 'branches',
+            match: {
+              _deleted: false,
+            },
+          },
+        })
+        .find(mongooseQuery);
+      // Executing
+      fullQuery.count()
         .exec()
-        .then((services) => {
+        .then((cnt) => {
           if (cnt === 0) {
-            return next([Strings.searchErrors.emptySearchResult]);
+            throw new Error(Strings.searchErrors.emptySearchResult);
           }
+          return fullQuery.populate('_business categories')
+            .select('name shortDescription _business.name _avgRating categories.title')
+            .skip(offset * 10)
+            .limit(10)
+            .exec();
+        })
+        .then((services) => {
           return res.json({
             count: cnt,
             results: services,
           });
         })
-        .catch(err => next([err]));
-    });
-});
+        .catch((err) => next([err]));
 
-/**
- *  Error Handling Middlewares.
- */
+      /**
+       *  Error Handling Middlewares.
+       */
 
-router.use((err, req, res, next) => {
-  res.status(400)
-    .json({
-      errors: err,
-    });
-});
+      router.use((err, req, res, next) => {
+        res.status(400)
+          .json({
+            errors: err,
+          });
+      });
 
-module.exports = router;
+      module.exports = router;
