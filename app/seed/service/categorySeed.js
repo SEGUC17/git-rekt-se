47a2b99967437f8c53ddcbaa6823794643088e65
Omarkojak <omarkojaks@gmail.com>
2017-04-02 17:03:54 +0200

    feat: Adding seeds to populate the database

---
+++ app/seed/service/categorySeed.js
@@ -0,0 +1,25 @@
+/**
+ * Categories Database Seed
+ */
+
+const Categories = [{
+  type: 'Business',
+  title: 'Language Courses',
+}, {
+  type: 'Business',
+  title: 'Self-Managment Courses',
+}, {
+  type: 'Business',
+  title: 'Team Management Courses',
+}, {
+  type: 'Service',
+  title: 'English Language Course',
+}, {
+  type: 'Serivce',
+  title: 'Personal Effectiveness',
+}, {
+  type: 'Serivce',
+  title: 'Principles of Self Management',
+}];
+
+module.exports = Categories;
---
+++ app/seed/service/serviceSeed.js
@@ -0,0 +1,19 @@
+/**
+ * Service Database Seed
+ */
+
+const Services = [{
+  name: 'Not Courses English Language Course',
+  shortDescription: 'We teach you English from scratch',
+  _business: null,
+  categories: null,
+  coverimage: 'notcourses.jpg',
+}, {
+  name: 'Enhance Personal Effectiveness Course',
+  shortDescription: '',
+  _business: null,
+  categories: null,
+  coverimage: 'enhance.jpg',
+}];
+
+module.exports = Services;
