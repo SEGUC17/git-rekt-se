3a8d9f13a8b476fbae097a3d1eac75abe5f45485
Omarkojak <omarkojaks@gmail.com>
2017-04-04 15:37:42 +0200

    refactor: changing routes file to resolve the conflicts

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,14 +1,14 @@
 const clientAuthAPI = require('./api/v1/client/auth');
 const businessAuthAPI = require('./api/v1/business/auth');
 const businessAPI = require('./api/v1/business/business');
+const businessGeneralAPI = require('./api/v1/business/general');
 const adminAuthAPI = require('./api/v1/admin/auth');
-const businessAPI = require('./api/v1/business/general');
 
 module.exports = (app) => {
   /**
    * Visitor Routes
    */
-  app.use('/api/v1/business', businessAPI);
+  app.use('/api/v1/business', businessGeneralAPI);
   /**
    * Client Routes
    */
