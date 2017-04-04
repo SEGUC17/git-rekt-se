a96f8fc816865cd4cbe74b57cb6a340fcceb2ce5
Hejazi <Hejazi@Hejazis-MacBook-Pro.local>
2017-04-04 12:53:58 +0200

    feat: not completed

--- app/routes/api/v1/service/serviceRoutes.js
+++ app/routes/api/v1/service/serviceRoutes.js
@@ -16,9 +16,26 @@ router.get('/:id', (req, res, next) => {
   Service.findOne({
     _id: req.params.id,
   })
-    .populate('_business branches categories')
+    .populate('_business branches categories reviews')
     .exec()
     .then((service) => {
+      const returnedService = {
+        name: service.name,
+        shortDescription: service.shortDescription,
+        description: service.description,
+        businessName: service.business.name,
+        businessEmail: service.business.service,
+        businessShortDescription: service.business.shortDescription,
+        businessDescription: service.business.description,
+        businessPhoneNumbers: service.business.phoneNumbers,
+        businessGallery: service.business.gallery,
+        businessWorkingHours: service.business,
+        // branches:
+        // offerings:
+        // reviews:
+        // gallery
+
+      };
       res.json(service);
     })
     .catch((err) => {
