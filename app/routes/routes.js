f3f382587cb832d8bbb0c6f20b58e67e3aed00a8
Mazen <mazennnnnnnnn@gmail.com>
2017-04-05 16:04:28 +0200

    fix: Resolved conflicts
    References #15.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,11 +3,8 @@ const businessAuthAPI = require('./api/v1/business/auth');
 const businessAPI = require('./api/v1/business/business');
 const businessGeneralAPI = require('./api/v1/business/general');
 const adminAuthAPI = require('./api/v1/admin/auth');
-<<<<<<< HEAD
 const ServiceGeneralAPI = require('./api/v1/service/general');
-=======
 const clientProfileAPI = require('./api/v1/client/profile');
->>>>>>> b2910c31f80132540be2e5505cfce6f1ffaf4d0b
 
 module.exports = (app) => {
   /**
