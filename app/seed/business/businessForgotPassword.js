9a61aeff2fb839ccaa28dfc10c264e50fe328a8b
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-05 12:30:47 +0200

    fix: Add Consistant Seed data

--- app/seed/business/businessForgotPassword.js
+++ app/seed/business/businessForgotPassword.js
@@ -1,10 +1,9 @@
-const locations = require('../service/locations');
 
 module.exports = [{
   name: 'Balabizo',
   email: 'hadyyasser23@gmail.com',
   shortDescription: 'This item is for testing the Business SignUp API',
-  phoneNumbers: ['12345677', '22222222', '32414553'],
+  phoneNumbers: ['01097167235', '01007167235', '01197167235'],
   password: 'blahblah1',
   confirmPassword: 'blahblah1',
   description: 'This is for testing the API',
