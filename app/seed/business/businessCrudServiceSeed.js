9f73ba7540f04f075daddbdf946524a7ceff79e8
Omarkojak <omarkojaks@gmail.com>
2017-04-06 10:13:21 +0200

    feat: adding seeds for testing

---
+++ app/seed/business/businessCrudServiceSeed.js
@@ -0,0 +1,31 @@
+/**
+ * Verified businesses Database seed for Services CRUD
+ */
+
+const Businesses = [{
+  name: 'Enhance',
+  email: 'enhance@gmail.com',
+  shortDescription: 'Self developpment is our target.',
+  categories: null,
+  password: '12345678H',
+  _status: 'verified',
+},
+{
+  name: 'Not Courses',
+  email: 'notcourses@gmail.com',
+  shortDescription: 'Teaching English Courses',
+  categories: null,
+  password: 'A12345678A',
+  _status: 'verified',
+},
+{
+  name: 'GUC german center',
+  email: 'gucgermancenter@gmail.com',
+  shortDescription: 'Teaching German Courses',
+  categories: null,
+  password: 'uefn12345678',
+  _status: 'verified',
+},
+];
+
+module.exports = Businesses;
