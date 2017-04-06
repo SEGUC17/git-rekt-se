508d3bd758946fe3573ddd59492780b5fca1bdcd
Medo3G <medo3g@yahoo.com>
2017-04-06 16:28:06 +0200

    test: Add search specific services seed

---
+++ app/seed/service/searchServicesSeed.js
@@ -0,0 +1,117 @@
+/**
+ * Services Seed For Search Tests
+*/
+
+const services = [{
+  name: 'German Course',
+  shortDescription: '3000 NC - 2000 Z',
+  _avgRating: 7,
+},
+{
+  name: 'English Course',
+  shortDescription: '5000 T - 7000 T',
+  _avgRating: 10,
+},
+{
+  name: 'French Course',
+  shortDescription: '4000 SZ',
+  _avgRating: 10,
+},
+{
+  name: 'Presentation Session',
+  shortDescription: '1500 6O - 4000Z',
+  _avgRating: 8,
+},
+{
+  name: 'Italian Lesson',
+  shortDescription: 'DELETED',
+  _avgRating: 10,
+  _deleted: true,
+},
+{
+  name: 'Self-Development Stuff',
+  shortDescription: '3000 T',
+  _avgRating: 8,
+},
+{
+  name: 'Test',
+  shortDescription: '2000 Z',
+  _avgRating: 7,
+},
+{
+  name: 'Test2',
+  shortDescription: '10000 Z',
+  _avgRating: 9,
+},
+{
+  name: 'Test3',
+  shortDescription: '5000 6O',
+  _avgRating: 7,
+},
+{
+  name: 'Japanese Lesson',
+  shortDescription: '600 Z',
+  _avgRating: 10,
+},
+{
+  name: 'Tagamo3 Pagination Test1',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test2',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test3',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test4',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test5',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test6',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test7',
+  shortDescription: 'Tagamo3 1st Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test8',
+  shortDescription: 'Tagamo3 2nd Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test9',
+  shortDescription: 'Tagamo3 2nd Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test10',
+  shortDescription: 'Tagamo3 2nd Page',
+  _avgRating: 7,
+},
+{
+  name: 'Tagamo3 Pagination Test11',
+  shortDescription: 'Tagamo3 2nd Page',
+  _avgRating: 7,
+},
+{
+  name: 'English Course',
+  shortDescription: '2000 6O',
+  _avgRating: 9,
+}];
+
+module.exports = services;
