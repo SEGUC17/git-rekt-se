2c1fc434d2f9cb1d668e4be1c9f778d0a61cfe85
Medo3G <medo3g@yahoo.com>
2017-04-06 16:56:42 +0200

    test: Add search specific offerings seed
    - Add those offerings to their relavant services in the services seed.

--- app/seed/service/searchServicesSeed.js
+++ app/seed/service/searchServicesSeed.js
@@ -1,3 +1,4 @@
+const offeringsSeed = require('./searchOfferingsSeed');
 /**
  * Services Seed For Search Tests
 */
@@ -6,112 +7,181 @@ const services = [{
   name: 'German Course',
   shortDescription: '3000 NC - 2000 Z',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[0],
+    offeringsSeed[1],
+  ],
 },
 {
   name: 'English Course',
   shortDescription: '5000 T - 7000 T',
   _avgRating: 10,
+  offerings: [
+    offeringsSeed[2],
+    offeringsSeed[3],
+  ],
 },
 {
   name: 'French Course',
   shortDescription: '4000 SZ',
   _avgRating: 10,
+  offerings: [
+    offeringsSeed[4],
+  ],
 },
 {
   name: 'Presentation Session',
   shortDescription: '1500 6O - 4000Z',
   _avgRating: 8,
+  offerings: [
+    offeringsSeed[5],
+    offeringsSeed[6],
+  ],
 },
 {
   name: 'Italian Lesson',
   shortDescription: 'DELETED',
   _avgRating: 10,
   _deleted: true,
+  offerings: [
+    offeringsSeed[7],
+  ],
 },
 {
   name: 'Self-Development Stuff',
   shortDescription: '3000 T',
   _avgRating: 8,
+  offerings: [
+    offeringsSeed[8],
+  ],
 },
 {
   name: 'Test',
   shortDescription: '2000 Z',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[1],
+  ],
 },
 {
   name: 'Test2',
   shortDescription: '10000 Z',
   _avgRating: 9,
+  offerings: [
+    offeringsSeed[9],
+  ],
 },
 {
   name: 'Test3',
   shortDescription: '5000 6O',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[10],
+  ],
 },
 {
   name: 'Japanese Lesson',
   shortDescription: '600 Z',
   _avgRating: 10,
+  offerings: [
+    offeringsSeed[11],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test1',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test2',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test3',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test4',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test5',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test6',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test7',
   shortDescription: 'Tagamo3 1st Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test8',
   shortDescription: 'Tagamo3 2nd Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test9',
   shortDescription: 'Tagamo3 2nd Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test10',
   shortDescription: 'Tagamo3 2nd Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'Tagamo3 Pagination Test11',
   shortDescription: 'Tagamo3 2nd Page',
   _avgRating: 7,
+  offerings: [
+    offeringsSeed[2],
+  ],
 },
 {
   name: 'English Course',
   shortDescription: '2000 6O',
   _avgRating: 9,
+  offerings: [
+    offeringsSeed[12],
+  ],
 }];
 
 module.exports = services;
