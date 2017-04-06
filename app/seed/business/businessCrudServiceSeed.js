24732c07bb430d474a52c14af3d7ec59d79b8820
Omarkojak <omarkojaks@gmail.com>
2017-04-06 11:06:13 +0200

    test: update seeds for testing

--- app/seed/business/businessCrudServiceSeed.js
+++ app/seed/business/businessCrudServiceSeed.js
@@ -6,25 +6,25 @@ const Businesses = [{
   name: 'Enhance',
   email: 'enhance@gmail.com',
   shortDescription: 'Self developpment is our target.',
-  categories: null,
   password: '12345678H',
   _status: 'verified',
+  branches: null,
 },
 {
   name: 'Not Courses',
   email: 'notcourses@gmail.com',
   shortDescription: 'Teaching English Courses',
-  categories: null,
   password: 'A12345678A',
   _status: 'verified',
+  branches: null,
 },
 {
   name: 'GUC german center',
   email: 'gucgermancenter@gmail.com',
   shortDescription: 'Teaching German Courses',
-  categories: null,
   password: 'uefn12345678',
   _status: 'verified',
+  branches: null,
 },
 ];
 
