e4dc8f574442cc189f33bc13dd6e9a3e81bf3d81
Hejazi <atherkhalid158@gmail.com>
2017-04-14 01:43:05 +0200

    fix: latest updates

--- public/dist/services/routes.js
+++ public/dist/services/routes.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 121);
+/******/ 	return __webpack_require__(__webpack_require__.s = 124);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -2503,7 +2503,7 @@ var router = new __WEBPACK_IMPORTED_MODULE_0_vue_router__["a" /* default */]({
 
 /***/ }),
 
-/***/ 121:
+/***/ 124:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(12);
