65f033714d3125ebfe5f87a52cb7c8ef7cea66a5
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-13 22:28:45 +0200

    feat: Add Bulma Card Vue Component

---
+++ public/src/components/shared/gr-card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card">
+        <div class="card-image" v-if="image">
+            <figure class="image is-4by3">
+                <img :src="image" alt="Image">
+            </figure>
+        </div>
+        <div class="card-content">            
+            <div class="content">
+               <slot name="content"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.card{
+    margin: 1em;
+}
+</style>
+<script>
+    export default {
+        props: {
+            image: {
+                type: String,
+                default: '',
+            }
+        },
+    }
+</script>
