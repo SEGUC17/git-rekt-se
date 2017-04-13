0062c7bc47e9e924f3a1ad186a054adfe905fd1a
Mazen <mazennnnnnnnn@gmail.com>
2017-04-14 01:46:08 +0200

    feat: Added more EndPoints
    References #72 and All Issues.

--- public/src/services/EndPoints.js
+++ public/src/services/EndPoints.js
@@ -1,6 +1,15 @@
 const BASE = 'http://localhost:3000/api/v1';
 
 export default {
+  Visitor() {
+    return {
+      search: `${BASE}/visitor/search/`,
+      viewService: serviceID => `${BASE}/service/${serviceID}`,
+      relatedService: (serviceID, offset) => `${BASE}/service/category/${serviceID}/${offset}`,
+      relatedBusiness: (businessID, offset) => `${BASE}/business/category/${businessID}/offset`,
+      viewBusiness: businessID => `${BASE}/business/${businessID}`,
+    };
+  },
   Client() {
     const authBase = `${BASE}/client/auth`;
     const profileBase = `${BASE}/client/profile`;
