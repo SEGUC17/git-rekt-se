6a13cabbf615d1d53fd6b3294a37f2c5f04a6f08
Mazen <mazennnnnnnnn@gmail.com>
2017-04-13 18:16:49 +0200

    feat: Added Client Sign Up View and English Language to Element
    References #72.

--- public/src/assets/js/app.js
+++ public/src/assets/js/app.js
@@ -1,6 +1,8 @@
+import Bulma from 'bulma';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import elementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
 import axios from 'axios';
 import sample from '../../components/sample.vue';
 import router from '../../services/routes.js';
@@ -8,12 +10,15 @@ import router from '../../services/routes.js';
 Window.axios = axios;
 
 Vue.use(VueRouter);
-Vue.use(elementUI);
+Vue.use(elementUI, {
+  locale
+});
 
 new Vue({
   el: '#root',
   router,
   components: {
     sample,
+    client: require('../../components/client/signup')
   }
 });
---
+++ public/src/components/client/signup.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="columns is-mobile">
+    <div class="column is-half is-offset-one-quarter">
+  
+      <h1 class="title has-text-centered">Sign Up</h1>
+  
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="First Name">
+          <el-input v-model="form.firstName" placeholder="A Name"></el-input>
+        </el-form-item>
+  
+        <el-form-item label="Last Name">
+          <el-input v-model="form.lastName" placeholder="Another Name"></el-input>
+        </el-form-item>
+  
+        <el-form-item label="Email">
+          <el-input v-model="form.email" placeholder="generic_email@somedomain.com"></el-input>
+        </el-form-item>
+  
+        <el-form-item label="Password">
+          <el-input v-model="form.password" placeholder="********"></el-input>
+        </el-form-item>
+  
+        <el-form-item label="Confirm Password">
+          <el-input v-model="form.confirmPassword" placeholder="********"></el-input>
+        </el-form-item>
+  
+        <el-form-item label="Mobile">
+          <el-input v-model="form.mobile" placeholder="01001234567"></el-input>
+        </el-form-item>
+        
+        <el-form-item label="Gender">
+          <el-radio-group v-model="form.gender">
+            <el-radio label="Male"></el-radio>
+            <el-radio label="Female"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="Birthdate">
+          <el-date-picker v-model="form.birthdate" type="date" :format="'dd-MM-yyyy'" placeholder="1/1/1990"></el-date-picker>
+        </el-form-item>
+  
+        <el-form-item class="has-text-centered">
+          <el-button type="primary" icon="circle-check" @click="onClick">Sign Up</el-button>
+        </el-form-item>
+      </el-form>
+  
+    </div>
+  </div>
+</template>
+
+<script>
+  import Form from '../../services/Form';
+  export default {
+    data() {
+      return {
+        form: new Form({
+          email: '',
+          password: '',
+          confirmPassword: '',
+          firstName: '',
+          lastName: '',
+          gender: '',
+          mobile: '',
+          birthdate: ''
+        }),
+      }
+    },
+    methods: {
+      onClick(){
+        console.log(this.form.data());
+      },
+    },
+  }
+</script>
