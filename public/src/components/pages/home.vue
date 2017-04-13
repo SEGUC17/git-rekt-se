b5ca837f987b5c2ff0364eed729e85e1caa5fa0c
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-13 22:29:03 +0200

    feat: Home Component Cards

--- public/src/components/pages/home.vue
+++ public/src/components/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="about">
+    <div class="home">
         <section class="gr-content hero is-bold has-text-centered">
             <div class="hero-body">
                 <div class="container intro-container">
@@ -42,29 +42,76 @@
                     <div class="column is-2">
                         <div class="field has-text-left">
                             <button class="button is-primary">
-                                          Search
-                                    </button>
+                                                                      Search
+                                                                </button>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+    
+        <div class="how-it-works">
+            <h3 class="title is-1 has-text-centered">
+                How it Works
+            </h3>
+            <p class="subtitle has-text-centered">
+                Bringing students and mentors together.
+            </p>
+    
+            <div class="columns">
+                <card class="column is-3 is-offset-1" image="http://localhost:3000/assets/imgs/home/search-online.svg">
+                    <div slot="content" class="has-text-centered">
+                        <h3>
+                            Find Businesses
+                        </h3>
+                        <p>
+                            Discover &amp; connect with great local businesses in your local neighborhood like dentists, hair stylists and more.
+                        </p>
+                    </div>
+                </card>
+    
+                <card class="column is-3" image="http://localhost:3000/assets/imgs/home/find-listing.svg">
+                    <div slot="content" class="has-text-centered">
+                        <h3>
+                            Find Service
+                        </h3>
+                        <p>
+                            Discover &amp; connect with great local businesses in your local neighborhood like dentists, hair stylists and more.
+                        </p>
+                    </div>
+                </card>
+    
+                <card class="column is-3" image="http://localhost:3000/assets/imgs/home/make-online-booking.svg">
+                    <div slot="content" class="has-text-centered">
+                        <h3>
+                            Book Online
+                        </h3>
+                        <p>
+                            Discover &amp; connect with great local businesses in your local neighborhood like dentists, hair stylists and more.
+                        </p>
+                    </div>
+                </card>
+    
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import card from '../shared/gr-card.vue';
     export default {
-    
+        components: {
+            card,
+        }
     };
 </script>
 
 <style>
-
-    .extra-large{
+    .extra-large {
         font-size: 4em;
         line-height: 1.2em;
     }
-
+    
     .intro-container {
         padding: 5em;
     }
@@ -77,6 +124,12 @@
     
     .gr-content {
         background-image: url('http://localhost:3000/assets/imgs/idx_bg.jpg');
-        margin-bottom: 0.5em;
+        margin-bottom: 2em;
+    }
+    
+    .how-it-works {
+        padding: 0 3em;
+        margin-top: 2em;
+        margin-bottom: 2em;
     }
 </style>
