a6f5deffac55445f15cd7398759a1cdecce6af8b
yousseffarahat <yousseffarahat@live.com>
2017-04-15 16:52:30 +0200

    fix:error handling

--- app/routes/api/v1/admin/businessRemoval.js
+++ app/routes/api/v1/admin/businessRemoval.js
@@ -33,6 +33,10 @@ router.post('/delete/:id', AdminAuth, (req, res, next) => {
   Service.find({
     _business: req.params.id,
   }, (err, results) => {
+    if (err) {
+      next(err);
+      return;
+    }
     for (i; i < results.length; i += 1) {
       results[i]._deleted = true;
     }
@@ -40,6 +44,10 @@ router.post('/delete/:id', AdminAuth, (req, res, next) => {
   Branch.find({
     _business: req.params.id,
   }, (err2, results) => {
+    if (err2) {
+      next(err2);
+      return;
+    }
     for (i; i < results.length; i += 1) {
       branchlist[i] = results[i]; // save to a const to not create a function inside this loop
       results[i]._deleted = true;
@@ -50,15 +58,19 @@ router.post('/delete/:id', AdminAuth, (req, res, next) => {
     Offering.find({
       branch: branch.id,
     }, (err3, results2) => {
+      if (err3) {
+        next(err3);
+        return;
+      }
       for (i; i < results2.length; i += 1) {
         results2[i]._deleted = true;
       }
     });
   });
 
-  Business.findByIdAndRemove(req.params.id, (err) => {
-    if (err) {
-      return next(err);
+  Business.findByIdAndRemove(req.params.id, (err4) => {
+    if (err4) {
+      return next(err4);
     }
     return res.json({
       message: Strings.adminSuccess.businessDeleted,
