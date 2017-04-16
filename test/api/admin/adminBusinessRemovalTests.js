c1142154f97d91571a96e5b1d97891036ef2ea4a
yousseffarahat <yousseffarahat@live.com>
2017-04-16 12:01:19 +0200

    feat: added pasing test

--- test/api/admin/adminBusinessRemovalTests.js
+++ test/api/admin/adminBusinessRemovalTests.js
@@ -3,6 +3,7 @@ const path = require('path');
 const supertest = require('supertest');
 const app = require('../../../app/app');
 const Business = require('../../../app/models/business/Business');
+const BusinessesSeed = require('../../../app/seed/business/confirmBusinessSeeds');
 const Admin = require('../../../app/models/admin/Admin');
 
 /**
@@ -44,6 +45,41 @@ describe('Category CRUD Test Suite', () => {
     Business.collection.drop(() => {
       Business.ensureIndexes(done);
     });
+  });
+  it('should delete a business and return a confirmation message', (done) => {
+    const business = BusinessesSeed[1];
+    new Business(business)
+      .save((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          Business.findOne({
+            email: business.email,
+          })
+            .exec((findErr, data) => {
+              if (findErr) {
+                done(findErr);
+              } else {
+                req = supertest(app)
+                  .post(`/api/v1/admin/business/delete/${data._id}`)
+                  .set('Authorization', `JWT ${token}`)
+                  .send()
+                  .expect('Content-Type', /json/)
+                  .expect(200)
+                  .end((confirmErr2, result2) => {
+                    if (confirmErr2) {
+                      // chai.expect(result2.body.message)
+                      //   .to.equal('Business was already confirmed.');
+                      done(confirmErr2);
+                    } else {
+                      console.log('working..');
+                      done();
+                    }
+                  });
+              }
+            });
+        }
+      });
   });
   it('should not do anything on invalid ID input', (done) => {
     req = supertest(app)
