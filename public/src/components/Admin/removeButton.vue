d1d73de4bb0449cf10b1d4e84b40619015f344a3
yousseffarahat <yousseffarahat@live.com>
2017-04-16 22:56:03 +0200

    feat: added button component

---
+++ public/src/components/Admin/removeButton.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+    
+    };
+</script>
+
+<style>
+    
+    .white {
+        color: #fff;
+    }
+    
+    .footer-icons {
+        padding: 5px;
+    }
+</style>
