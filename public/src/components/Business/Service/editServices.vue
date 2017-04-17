cd4c8be309b3ecc6332077455f183ccab5030c00
Medo3G <medo3g@yahoo.com>
2017-04-17 16:06:01 +0200

    fix: Contnue create serivice

--- public/src/components/Business/Service/editServices.vue
+++ public/src/components/Business/Service/editServices.vue
@@ -5,12 +5,14 @@
         <div slot="header" class="clearfix">
           <span>Create Service</span>
         </div>
+        <el-alert v-if:"createSuccess" title="Success" type="success" :description="createSuccess" show-icon></el-alert>
+        <el-alert v-for"error in errors" :key"error" :title="Error" type="error" :description="error" show-icon></el-alert>
         <el-form ref="createService" :model="newService" :rules="ServiceRules" :label-position="left">
           <el-form-item label="Name" required prop="name">
             <el-input v-model="service.name" placeholder="The name of your service"></el-input>
           </el-form-item>
           <el-form-item label="Short Description" required prop="shortDescription">
-            <el-input type="textarea" v-model="service.shortDescription" placeholder="A brief description of your service"></el-input>
+            <el-input type="textarea" v-model="service.shortDescription" placeholder="A brief description of your service (Max 140 characters)"></el-input>
           </el-form-item>
           <el-form-item label="Description">
             <el-input type="textarea" v-model="service.shortDescription" placeholder="A detailed description of your service"></el-input>
@@ -23,7 +25,7 @@
           </el-form-item>
           <!--TODO Upload Cover Image-->
           <el-form-item>
-            <el-button type="primary" @click="submitForm('createService')">Create</el-button>
+            <el-button type="primary" @click="createService">Create</el-button>
             <el-button @click="resetForm('createService')">Reset</el-button>
           </el-form-item>
         </el-form>
@@ -47,16 +49,12 @@
         newService: {},
         generalErrors: [],
         serviceRules: ServiceRules,
+        createSuccess: '',
+        createErrors: [],
       };
     },
     mounted() {
-      Axios.get(Business().listServices)
-        .then((response) => {
-          this.services = response.data;
-        })
-        .catch((err) => {
-          this.generalErrors = err.response.data.errors;
-        });
+      this.getServices();
       Axios.get(Business().listCategories)
         .then((response) => {
           this.categories = response.data;
@@ -64,6 +62,37 @@
         .catch((err) => {
           this.generalErrors = err.response.data.errors;
         });
+    },
+    methods: {
+      getServices() {
+        Axios.get(Business().listServices)
+        .then((response) => {
+          this.services = response.data;
+        })
+        .catch((err) => {
+          this.generalErrors = err.response.data.errors;
+        });
+      },
+      createService() {
+        this.$refs.createService.validate((valid) => {
+          if (valid) {
+            Axios.post(Business().create, this.newService)
+            .then((response) => {
+              this.createErrors = [];
+              this.createSuccess = response.data.message;
+              this.resetForm('createService');
+              this.getServices();
+            })
+            .catch((err) => {
+              this.createSuccess = '';
+              this.createErrors = err.response.data.errors;
+            });
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
     },
   };
 </script>
