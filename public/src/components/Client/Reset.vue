ba0f769eddfad5c3bbb081910e658f68eb79ea61
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-18 15:03:45 +0200

    feat: Add proper error handling

--- public/src/components/Client/Reset.vue
+++ public/src/components/Client/Reset.vue
@@ -6,7 +6,7 @@
             <div class="hero-body">
                 <div class="container">
                     <h1 class="title extra-large white">
-                        Forgot Password
+                        Reset Password
                     </h1>
                 </div>
             </div>
@@ -14,11 +14,23 @@
 
         <div class="reset-password-form columns">
             <div class="column content is-8 is-offset-2">
-                <el-alert title="Password changed successfully" v-show="form.success" type="success" show-icon>
-                </el-alert>
+               <div class="alerts">
+                
+                <el-alert class="alert-msg" :title="form.message" v-show="form.success" type="success" show-icon></el-alert>
 
-                <el-alert title="Token expired" v-show="form.fail" type="error" show-icon>
-                </el-alert>
+                <!--<el-alert class="alert-msg" title="Token expired" v-show="form.fail" type="error" show-icon></el-alert>-->
+                <div class="alert-msg error" v-for="key in form.keys" v-show="form.errors.has(key)">
+                        <el-alert @close="form.errors.remove(key)" type="error"
+                                  :title="form.errors.getAll(key, ' | ') || '' " show-icon></el-alert>
+                </div>
+
+                <div class="alert-msg error" v-show="form.errors.has('serverError')">
+                        <el-alert @close="form.errors.remove('serverError')"
+                                  :title="form.errors.getAll('serverError', ' | ') || ''" type="error"
+                                  show-icon></el-alert>
+                </div>
+
+               </div>
 
                 <div class="login-form">
                     <el-form :model="form" :rules="rules" ref="form">
@@ -48,7 +60,7 @@
                         </el-form-item>
 
                         <el-form-item>
-                            <el-button type="primary" @click="submitForm('form')">Submit</el-button>
+                            <el-button type="primary" :loading="loading" @click="submitForm('form')">Submit</el-button>
                         </el-form-item>
                     </el-form>
                 </div>
@@ -76,9 +88,11 @@
           confirmPassword: '',
           success: false,
           fail: false,
+          message: '',
         }),
         showPassword: 'password',
         showConfirm: 'password',
+        loading: false,
         errors: {},
         rules: clientForgotPassword,
       };
@@ -87,12 +101,19 @@
       submitForm(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
+            this.loading = true;
             this.form.post(Client().reset)
-                .then(() => {
+                .then((data) => {
                   this.form.success = true;
+                  this.loading = false;
+                  this.form.message = data.message;
                 })
-                .catch(() => {
+                .catch((e) => {
                   this.form.fail = true;
+                  this.loading = false;
+                  this.errors = e.map(() => {
+
+                  });
                 });
           }
         });
@@ -102,6 +123,10 @@
 </script>
 
 <style>
+    .alert-msg{
+        margin-top: 1em;
+    }
+
     .client-forgot-top{
         background: #159957;  /* fallback for old browsers */
         background: -webkit-linear-gradient(to right, #155799, #159957);  /* Chrome 10-25, Safari 5.1-6 */
