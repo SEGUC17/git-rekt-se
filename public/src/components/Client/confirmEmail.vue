b4988f94c09350de95ca1b453318e778823f52bf
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-20 00:45:23 +0200

    feat: Add Resend on confirmation failure

--- public/src/components/Client/confirmEmail.vue
+++ public/src/components/Client/confirmEmail.vue
@@ -1,43 +1,106 @@
 <template>
     <div class="confirm-email">
+        <div class="email-confirmed">
+            <div class="hero">
+                <div class="container">
+                    <div class="hero-body has-text-centered">
+                        <el-icon name="circle-check" class="confirmation-icon" v-if="confirmed"></el-icon>
+                        <el-icon name="circle-close" class="confirmation-icon icon-fail"
+                                 v-if="errors.length > 0"></el-icon>
 
-        <div class="container has-text-centered">
-            <h1 class="title extra-large white">
-                Email Confirmed
-            </h1>
-            <p class="subtitle white">
-                Thanks for confirming your email, you can now enjoy the features of a registered user.
-            </p>
+                        <h2 class="title is-2" v-if="confirmed">
+                            Email confirmed!
+                        </h2>
+
+                        <div class="errors" v-if="errors.length > 0">
+                            <h2 class="title is-3" v-for="error in errors" :key="error"> {{ error }} </h2>
+                        </div>
+
+                        <div class="columns" v-if="!resend && !confirmed">
+                            <div class="column is-6 is-offset-3">
+                                <div class="resend-email">
+                                    <el-form :model="resendForm" :rules="emailRule" ref="resendForm">
+                                        <el-form-item prop="email">
+                                            <el-input placeholder="Email" v-model="resendForm.email"></el-input>
+                                        </el-form-item>
+                                    </el-form>
+                                </div>
+                                <el-button type="primary" size="large" @click="resendEmail('resendForm')">
+                                    Resend email?
+                                </el-button>
+                            </div>
+                        </div>
+
+                        <resend :email="resendForm.email" send="true" v-if="resend"></resend>
+
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
 </template>
 
 <script>
   import clientAuth from '../../services/auth/clientAuth';
+  import resend from '../Client/resend.vue';
 
   export default {
+    components: {
+      resend,
+    },
     data() {
       return {
         confirmationSuccess: '',
+        resendForm: {
+          email: '',
+        },
         confirmed: false,
+        resend: false,
         errors: [],
+        emailRule: {
+          email: [{
+            required: true,
+            message: 'Email is required.',
+            trigger: 'blur',
+          }, {
+            type: 'email',
+            message: 'Invalid Email format.',
+            trigger: 'blur',
+          }],
+        },
       };
     },
     mounted() {
+      const loader = this.$loading({
+        fullscreen: true,
+      });
+
       clientAuth.confirmEmail(this.$route.params.token, (responseErrs, response) => {
+        loader.close();
         if (responseErrs) {
           this.errors = responseErrs.errors;
         } else {
           this.confirmationSuccess = response.message;
           this.confirmed = true;
-//          setTimeout(() => {
-//            this.$router.push('/');
-//          }, 1000);
+          setTimeout(() => {
+            this.$router.push('/client/login');
+          }, 1000);
         }
       });
     },
-  };
+
+    methods: {
+      resendEmail(formName) {
+        this.$refs[formName].validate((valid) => {
+          console.log(valid);
+          if (valid) {
+            this.errors = [];
+            this.resend = true;
+          }
+        });
+      },
+    },
+  }
 </script>
 
 <style>
@@ -46,12 +109,17 @@
         padding: 2em;
     }
 
-    :root {
-        background: #67B26F !important; /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #4ca2cd, #67B26F) !important; /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to right, #4ca2cd, #67B26F) !important; /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    .errors {
+        margin: 2em;
     }
 
+    .confirmation-icon {
+        font-size: 10em;
+        color: #42d885;
+    }
 
+    .icon-fail {
+        color: #ff2b56;
+    }
 </style>
 
