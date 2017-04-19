4ee140a3f6a0cb14b2a8dfebf77d1420149726da
Mazen <mazennnnnnnnn@gmail.com>
2017-04-19 22:18:12 +0200

    feat: Added Date Column
    References #101.

--- public/src/components/Business/viewTransactions.vue
+++ public/src/components/Business/viewTransactions.vue
@@ -12,6 +12,11 @@
     </div>
 
     <el-table :data="bookings" border>
+      <template scope="scope">
+        <el-icon name="time"></el-icon>
+        <span>{{ scope.row.date }}</span>
+      </template>
+
       <el-table-column label="Service Name" prop="_service.name">
       </el-table-column>
   
