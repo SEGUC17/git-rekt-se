8ffd1d8942a2a03259d7ffd40140ba0c0c5b7e92
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-21 19:55:52 +0200

    fix: Remove dist files

--- public/src/components/Admin/removeBusiness.vue
+++ public/src/components/Admin/removeBusiness.vue
@@ -86,17 +86,3 @@
         },
     };
 </script>
-
-<style>
-    .error {
-        margin-top: 20px;
-    }
-    
-    .error:first-child {
-        margin-top: none;
-    }
-    
-    .demo-ruleForm {
-        margin-top: 30px;
-    }
-</style>
