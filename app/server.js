75d0706551ac735ee9273b8da74460d841476b4e
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-25 23:44:09 +0200

    feat: Add https server

--- app/server.js
+++ app/server.js
@@ -2,12 +2,28 @@
  * Require express main app.
  */
 
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const https = require('https');
 const app = require('./app');
 
 const port = process.env.PORT;
+const httpsPort = process.env.HTTPS_PORT;
 
 /**
  * Start server.
  */
 
-app.listen(port, () => console.log('Server started.\nListening on port 3000.'));
+/**
+ * Https Options
+ */
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname, './certs/fullchain.pem')),
+  ket: fs.readFileSync(path.join(__dirname, './certs/privkey.pem')),
+};
+
+// app.listen(port, () => console.log('Server started.\nListening on port 3000.'));
+
+http.createServer(app).listen(port);
+https.createServer(httpsOptions, app).listen(httpsPort);
