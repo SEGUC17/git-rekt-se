0c369f70f1a51470c206b59eea31911188647f94
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-04-30 07:15:57 +0200

    fix: Consistant styles

--- public/src/components/Business/reset.vue
+++ public/src/components/Business/reset.vue
@@ -3,7 +3,7 @@
     <gr-top-hero class="bus-signin-top" title="Reset Password"></gr-top-hero>
     <div class="columns is-mobile">
       <div class="column is-half-desktop is-10-mobile is-10-tablet
-                                 is-offset-1-mobile is-offset-1-tablet is-offset-one-quarter-desktop">
+                                   is-offset-1-mobile is-offset-1-tablet is-offset-one-quarter-desktop">
   
         <!-- Error Messages-->
         <div class="errors" v-if="errors.length > 0">
@@ -29,8 +29,8 @@
           <el-form-item label="Confirm Password" prop="confirmPassword">
             <el-input :type="showConfirm? 'text':'password'" v-model="form.confirmPassword" auto-complete="off">
               <div slot="append">
-               <el-button @click="showConfirm = !showConfirm"><i class="fa fa-eye"></i>
-               </el-button>
+                <el-button @click="showConfirm = !showConfirm"><i class="fa fa-eye"></i>
+                </el-button>
               </div>
             </el-input>
           </el-form-item>
@@ -45,90 +45,90 @@
 </template>
 
 <script>
-  /**
-   * This component allows a Business change his password
-   * after he requests to change it.
-   */
-  import Form from '../../services/Form';
-  import { Business } from '../../services/EndPoints';
-  import { BusinessResetFormValidation } from '../../services/validation';
-  import businessAuth from '../../services/auth/businessAuth';
-  import JWTCheck from '../../services/JWTErrors';
+/**
+ * This component allows a Business change his password
+ * after he requests to change it.
+ */
+import Form from '../../services/Form';
+import { Business } from '../../services/EndPoints';
+import { BusinessResetFormValidation } from '../../services/validation';
+import businessAuth from '../../services/auth/businessAuth';
+import JWTCheck from '../../services/JWTErrors';
 
-  export default {
+export default {
+  /**
+   * Data used by this component.
+   * form: Holds the data entered by user and sent to server.
+   * rules: Validation Rules used to validate the input.
+   * message: Message to view to the client.
+   * loading: true if a request is sent and is being processed by server,
+   * otherwise false.
+   * errors: Errors received from server.
+   * alert_show: true to show alert, false otherwise.
+   * error_show: true to show error, false otherwise.
+   * btn_disable: true to disable button, false otherwise.
+   */
+  data() {
+    BusinessResetFormValidation.confirmPassword[1]
+      .validator = BusinessResetFormValidation.confirmPassword[1].validator.bind(this);
+    BusinessResetFormValidation.password[2]
+      .validator = BusinessResetFormValidation.password[2].validator.bind(this);
+    return {
+      form: new Form({
+        password: '',
+        confirmPassword: '',
+        token: this.$route.params.token,
+      }),
+      rules: BusinessResetFormValidation,
+      message: '',
+      loading: false,
+      errors: [],
+      alert_show: false,
+      error_show: false,
+      btn_disable: true,
+      showPassword: false,
+      showConfirm: false,
+    };
+  },
+  /**
+   * Methods used by the component.
+   */
+  methods: {
     /**
-     * Data used by this component.
-     * form: Holds the data entered by user and sent to server.
-     * rules: Validation Rules used to validate the input.
-     * message: Message to view to the client.
-     * loading: true if a request is sent and is being processed by server,
-     * otherwise false.
-     * errors: Errors received from server.
-     * alert_show: true to show alert, false otherwise.
-     * error_show: true to show error, false otherwise.
-     * btn_disable: true to disable button, false otherwise.
+     * Validates and Submits a form.
      */
-    data() {
-      BusinessResetFormValidation.confirmPassword[1]
-        .validator = BusinessResetFormValidation.confirmPassword[1].validator.bind(this);
-      BusinessResetFormValidation.password[2]
-        .validator = BusinessResetFormValidation.password[2].validator.bind(this);
-      return {
-        form: new Form({
-          password: '',
-          confirmPassword: '',
-          token: this.$route.params.token,
-        }),
-        rules: BusinessResetFormValidation,
-        message: '',
-        loading: false,
-        errors: [],
-        alert_show: false,
-        error_show: false,
-        btn_disable: true,
-        showPassword: false,
-        showConfirm: false,
-      };
-    },
-    /**
-     * Methods used by the component.
-     */
-    methods: {
-      /**
-       * Validates and Submits a form.
-       */
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.loading = true;
-            this.form.post(Business().reset)
-              .then((data) => {
-                this.message = data.message;
-                this.alert_show = true;
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          this.form.post(Business().reset)
+            .then((data) => {
+              this.message = data.message;
+              this.alert_show = true;
+              this.error_show = false;
+              this.loading = false;
+              setTimeout(() => {
+                this.$router.push('/business/login');
+              }, 2000);
+            })
+            .catch((e) => {
+              if (e.response && JWTCheck(e.response.data.errors)) {
+                businessAuth.removeData();
+                this.$router.push('/');
+                this.$toast.open({
+                  text: 'Your sessions has expired. Please login.',
+                  position: 'bottom',
+                  type: 'danger',
+                });
+              } else {
+                this.alert_show = false;
                 this.error_show = false;
-                this.loading = false;
-                setTimeout(() => {
-                    this.$router.push('/business/login');
-                }, 2000);
-              })
-              .catch((e) => {
-                if (e.response && JWTCheck(e.response.data.errors)) {
-                  businessAuth.removeData();
-                  this.$router.push('/');
-                  this.$toast.open({
-                    text: 'Your sessions has expired. Please login.',
-                    position: 'bottom',
-                    type: 'danger',
-                  });
-                } else {
-                  this.alert_show = false;
-                  this.error_show = false;
-                  this.errors = e;
-                }
-              });
-          }
-        });
-      },
+                this.errors = e;
+              }
+            });
+        }
+      });
     },
-  };
+  },
+};
 </script>
--- public/src/components/Client/clientEditInfo.vue
+++ public/src/components/Client/clientEditInfo.vue
@@ -6,7 +6,7 @@
   
     <div class="columns is-mobile">
       <div class="column is-half-desktop is-10-mobile is-10-tablet
-                                           is-offset-1-mobile is-offset-1-tablet is-offset-one-quarter-desktop">
+                                             is-offset-1-mobile is-offset-1-tablet is-offset-one-quarter-desktop">
   
         <!-- Backend Form Errors and message(s) -->
         <div class="error" v-show="success">
@@ -77,103 +77,130 @@
           </el-form-item>
         </el-form>
   
-  
       </div>
     </div>
   </div>
 </template>
 
 <script>
+/**
+ * This component allows the client to Edit his Info.
+ */
+import axios from 'axios';
+import Form from '../../services/Form';
+import Errors from '../../services/Errors';
+import { Client } from '../../services/EndPoints';
+import clientAuth from '../../services/auth/clientAuth';
+import { clientEditInfoValidation } from '../../services/validation';
+import JWTCheck from '../../services/JWTErrors';
+import EventBus from '../../services/EventBus';
+
+export default {
   /**
-   * This component allows the client to Edit his Info.
+   * The data used by the component.
+   * form: The data that the user enters and is sent to the server.
+   * rules: The validation rules for validating user input.
+   * client: The client data fetched from server.
+   * showPassword: Used to show the user the password he/she entered.
+   * showConfirm: Used to show the user the password he/she entered.
+   * error: true if an error occured, false otherwise.
+   * success: true if a successfull operation was excuted, false otherwise.
+   * successMessage: Message received from the server.
    */
-  import axios from 'axios';
-  import Form from '../../services/Form';
-  import Errors from '../../services/Errors';
-  import { Client } from '../../services/EndPoints';
-  import clientAuth from '../../services/auth/clientAuth';
-  import { clientEditInfoValidation } from '../../services/validation';
-  import JWTCheck from '../../services/JWTErrors';
-  import EventBus from '../../services/EventBus';
-  
-  export default {
+  data() {
+    clientEditInfoValidation.confirmPassword[0].validator = clientEditInfoValidation
+      .confirmPassword[0].validator.bind(this);
+    clientEditInfoValidation.password[1].validator = clientEditInfoValidation.password[1]
+      .validator.bind(this);
+    clientEditInfoValidation.birthdate[1].validator = clientEditInfoValidation.birthdate[1]
+      .validator.bind(this);
+    return {
+      form: new Form({
+        email: '',
+        password: '',
+        confirmPassword: '',
+        firstName: '',
+        lastName: '',
+        gender: '',
+        mobile: '',
+        birthdate: '',
+      }),
+      rules: clientEditInfoValidation,
+      client: {},
+      loading: false,
+      showPassword: false,
+      showConfirm: false,
+      success: false,
+      error: false,
+      successMessage: '',
+      passwordChanged: false,
+      emailChanged: false,
+    };
+  },
+  /**
+   * Ran when component is mounted on DOM.
+   * Client is only allowed to edit info, if he is logged in.
+   * If client is not authenticated route him back with a message.
+   */
+  mounted() {
+    if (!clientAuth.isAuthenticated()) {
+      this.$router.push('/404');
+      return;
+    }
+    this.fillForm();
+  },
+  /**
+   * Methods Used by the component.
+   */
+  methods: {
     /**
-     * The data used by the component.
-     * form: The data that the user enters and is sent to the server.
-     * rules: The validation rules for validating user input.
-     * client: The client data fetched from server.
-     * showPassword: Used to show the user the password he/she entered.
-     * showConfirm: Used to show the user the password he/she entered.
-     * error: true if an error occured, false otherwise.
-     * success: true if a successfull operation was excuted, false otherwise.
-     * successMessage: Message received from the server.
+     * Fetches data from the server and displays it.
      */
-    data() {
-      clientEditInfoValidation.confirmPassword[0].validator = clientEditInfoValidation
-        .confirmPassword[0].validator.bind(this);
-      clientEditInfoValidation.password[1].validator = clientEditInfoValidation.password[1]
-        .validator.bind(this);
-      clientEditInfoValidation.birthdate[1].validator = clientEditInfoValidation.birthdate[1]
-        .validator.bind(this);
-      return {
-        form: new Form({
-          email: '',
-          password: '',
-          confirmPassword: '',
-          firstName: '',
-          lastName: '',
-          gender: '',
-          mobile: '',
-          birthdate: '',
-        }),
-        rules: clientEditInfoValidation,
-        client: {},
-        loading: false,
-        showPassword: false,
-        showConfirm: false,
-        success: false,
-        error: false,
-        successMessage: '',
-        passwordChanged: false,
-        emailChanged: false,
-      };
-    },
-    /**
-     * Ran when component is mounted on DOM.
-     * Client is only allowed to edit info, if he is logged in.
-     * If client is not authenticated route him back with a message.
-     */
-    mounted() {
-      if (!clientAuth.isAuthenticated()) {
-        this.$router.push('/404');
-        return;
-      }
-      this.fillForm();
-    },
-    /**
-     * Methods Used by the component.
-     */
-    methods: {
-      /**
-       * Fetches data from the server and displays it.
-       */
-      fillForm() {
-        const loader = this.$loading({
-          fullscreen: true,
+    fillForm() {
+      const loader = this.$loading({
+        fullscreen: true,
+      });
+      this.getClient()
+        .then(() => {
+          loader.close();
+          this.form.email = this.client.email;
+          this.form.confirmPassword = this.client.confirmPassword;
+          this.form.firstName = this.client.firstName;
+          this.form.lastName = this.client.lastName;
+          this.form.gender = this.client.gender;
+          this.form.mobile = this.client.mobile;
+          this.form.birthdate = new Date(this.client.birthdate);
+        })
+        .catch((err) => {
+          loader.close();
+          if (err.response && JWTCheck(err.response.data.errors)) {
+            clientAuth.removeData();
+            this.$router.push('/');
+            this.$toast.open({
+              message: 'Session Expired, please login',
+              type: 'is-danger',
+              position: 'bottom',
+            });
+          } else {
+            this.error = true;
+            this.message = err.response ? err.response.data.errors.join(' | ') : err.message;
+          }
         });
-        this.getClient()
-          .then(() => {
-            loader.close();
-            this.form.email = this.client.email;
-            this.form.confirmPassword = this.client.confirmPassword;
-            this.form.firstName = this.client.firstName;
-            this.form.lastName = this.client.lastName;
-            this.form.gender = this.client.gender;
-            this.form.mobile = this.client.mobile;
-            this.form.birthdate = new Date(this.client.birthdate);
-          })
-          .catch((err) => {
-            loader.close();
+    },
+    /**
+     * Fetch the client info.
+     */
+    getClient() {
+      return new Promise((resolve, reject) => {
+        axios.get(Client().getInfo(clientAuth.user.userID()), {
+          headers: {
+            Authorization: clientAuth.getJWTtoken(),
+          },
+        })
+          .then((response) => {
+            this.client = response.data;
+            resolve();
+          }).catch((err) => {
             if (err.response && JWTCheck(err.response.data.errors)) {
               clientAuth.removeData();
               this.$router.push('/');
@@ -185,23 +212,63 @@
             } else {
               this.error = true;
               this.message = err.response ? err.response.data.errors.join(' | ') : err.message;
+              reject(err);
             }
           });
-      },
-      /**
-       * Fetch the client info.
-       */
-      getClient() {
-        return new Promise((resolve, reject) => {
-          axios.get(Client().getInfo(clientAuth.user.userID()), {
+      });
+    },
+    /**
+     * Submit the form when the user clicks it.
+     */
+    submitForm(formName) {
+      this.success = false;
+      this.successMessage = '';
+      this.form.errors = new Errors();
+      if (this.form.password && this.form.confirmPassword) {
+        this.passwordChanged = true;
+      }
+      if (this.form.email !== clientAuth.user.userEmail()) {
+        this.emailChanged = true;
+      }
+
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          this.form.post(Client().editInfo(clientAuth.user.userID()), {
             headers: {
               Authorization: clientAuth.getJWTtoken(),
             },
           })
-            .then((response) => {
-              this.client = response.data;
-              resolve();
+            .then((data) => {
+              this.loading = false;
+              this.success = true;
+              this.successMessage = data.message;
+              this.fillForm();
+              if (this.passwordChanged || this.emailChanged) {
+                this.loading = true;
+                clientAuth.logout((responseErrs, response) => {
+                  let message;
+                  EventBus.$emit('UpdateNavigation');
+                  this.loading = false;
+                  if (responseErrs) {
+                    message = responseErrs.errors[0];
+                  } else {
+                    message = this.emailChanged ? 'Please wait for confirmation mail to login into the system' : 'Please login again';
+                  }
+
+                  this.$toast.open({
+                    message,
+                    type: 'is-primary',
+                    position: 'bottom',
+                  });
+
+                  this.$router.push({
+                    path: '/',
+                  });
+                });
+              }
             }).catch((err) => {
+              this.loading = false;
               if (err.response && JWTCheck(err.response.data.errors)) {
                 clientAuth.removeData();
                 this.$router.push('/');
@@ -213,108 +280,40 @@
               } else {
                 this.error = true;
                 this.message = err.response ? err.response.data.errors.join(' | ') : err.message;
-                reject(err);
               }
             });
-        });
-      },
-      /**
-       * Submit the form when the user clicks it.
-       */
-      submitForm(formName) {
-        this.success = false;
-        this.successMessage = '';
-        this.form.errors = new Errors();
-        if (this.form.password && this.form.confirmPassword) {
-          this.passwordChanged = true;
         }
-        if (this.form.email !== clientAuth.user.userEmail()) {
-          this.emailChanged = true;
-        }
-  
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.loading = true;
-            this.form.post(Client().editInfo(clientAuth.user.userID()), {
-              headers: {
-                Authorization: clientAuth.getJWTtoken(),
-              },
-            })
-              .then((data) => {
-                this.loading = false;
-                this.success = true;
-                this.successMessage = data.message;
-                this.fillForm();
-                if (this.passwordChanged || this.emailChanged) {
-                  this.loading = true;
-                  clientAuth.logout((responseErrs, response) => {
-                    let message;
-                    EventBus.$emit('UpdateNavigation');
-                    this.loading = false;
-                    if (responseErrs) {
-                      message = responseErrs.errors[0];
-                    } else {
-                      message = this.emailChanged ? 'Please wait for confirmation mail to login into the system' : 'Please login again';
-                    }
-  
-                    this.$toast.open({
-                      message,
-                      type: 'is-primary',
-                      position: 'bottom',
-                    });
-  
-                    this.$router.push({
-                      path: '/',
-                    });
-                  });
-                }
-              }).catch((err) => {
-                this.loading = false;
-                if (err.response && JWTCheck(err.response.data.errors)) {
-                  clientAuth.removeData();
-                  this.$router.push('/');
-                  this.$toast.open({
-                    message: 'Session Expired, please login',
-                    type: 'is-danger',
-                    position: 'bottom',
-                  });
-                } else {
-                  this.error = true;
-                  this.message = err.response ? err.response.data.errors.join(' | ') : err.message;
-                }
-              });
-          }
-        });
-      },
-      /**
-       * Reset Form to its original data.
-       */
-      resetForm() {
-        // No need to bind this since we are using arrow function.
-        this.form.keys.forEach((key) => {
-          if(this.client.hasOwnProperty(key)){
-            this.form[key] = this.client[key];
-          }
-        });
-      },
-      /**
-       * Checkes if form has errors.
-       */
-      hasErrors() {
-        const errors = this.$refs.form.$children.filter(el => el.validateMessage.length > 0);
-        return errors.length > 0;
-      },
+      });
     },
-  };
+    /**
+     * Reset Form to its original data.
+     */
+    resetForm() {
+      // No need to bind this since we are using arrow function.
+      this.form.keys.forEach((key) => {
+        if (this.client.hasOwnProperty(key)) {
+          this.form[key] = this.client[key];
+        }
+      });
+    },
+    /**
+     * Checkes if form has errors.
+     */
+    hasErrors() {
+      const errors = this.$refs.form.$children.filter(el => el.validateMessage.length > 0);
+      return errors.length > 0;
+    },
+  },
+};
 </script>
 
 <style>
-  .edit-information-client-top {
-    background: #141E30;
-    /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #243B55, #141E30);
-    /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #243B55, #141E30);
-    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  }
+.edit-information-client-top {
+  background: #141E30;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #243B55, #141E30);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #243B55, #141E30);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 </style>
